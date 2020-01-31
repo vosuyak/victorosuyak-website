f84a47473a0d7cd503e04d8e861e5ca90aae880c
vosuyak <vosyak@yahoo.com>
2020-01-31 01:08:53 -0500

    mongodb connected succesgully

--- backend-golang/src/education-microservices/data/mongodb.go
+++ backend-golang/src/education-microservices/data/mongodb.go
@@ -3,17 +3,18 @@ package data
 import (
 	"context"
 	"fmt"
-	"log"
-	"time"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
 )
 
 func init() {
 	GetClient()
 }
 func GetClient() *mongo.Client {
+	fmt.Println("Connect")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
