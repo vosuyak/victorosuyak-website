76351be133cd423a858f9193fa2b1a6c4c956a9d
vosuyak <vosyak@yahoo.com>
2020-02-01 12:20:54 -0500

    configuration set

--- backend-golang/src/education-microservices/data/mongodb.go
+++ backend-golang/src/education-microservices/data/mongodb.go
@@ -8,15 +8,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"time"
+	"education/core"
 )
 
 func init() {
+	core.LoadAppConfig()
 	GetClient()
 }
+
+// GetClient - Connect to Client
 func GetClient() *mongo.Client {
 	fmt.Println("Connect")
+	cf := core.AppConfig
+	fmt.Println(cf)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+cf.MgAddrs))
 	if err != nil {
 		log.Fatal(err)
 	}
