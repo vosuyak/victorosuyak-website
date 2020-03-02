802e3609f5c2a76b15546658d015670f64665059
vosuyak <vosyak@yahoo.com>
2020-03-01 19:33:59 -0500

    frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
 
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'education',
+    component: EducationComponent
   },
   { path: '',
     redirectTo: '/home',
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { EducationComponent } from './education/education.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SkillComponent } from './skill/skill.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    EducationComponent,
+    HeaderComponent,
+    SkillComponent,
+    ExperienceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
