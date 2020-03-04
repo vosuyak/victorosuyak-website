e42a52fa23b0646f98e20d70695106791c29d2ce
vosuyak <vosyak@yahoo.com>
2020-03-03 20:59:22 -0500

    sections scroll

---
+++ frontend/src/app/core/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  getEducation(){
+    let url = './../../assets/pages/education.json';
+    return this.http.get(url)
+  }
+}
--- frontend/src/app/education/education.component.ts
+++ frontend/src/app/education/education.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from './../core/data.service';
 @Component({
   selector: 'page-education',
   templateUrl: './education.component.html',
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class EducationComponent implements OnInit {
   page:string = 'education';
 
-  constructor() { }
+  constructor(private data:DataService) { }
 
   ngOnInit(): void {
+    // this.getEducation();
+  }
+
+  getEducation(){
+    this.data.getEducation().subscribe(data =>{
+      data
+      console.log('data: ', data);
+    })
   }
 
 }
